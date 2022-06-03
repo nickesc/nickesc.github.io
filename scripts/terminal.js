3da67995ac0e7ce54ef263d05d84e19e7bbce09b
N. Escobar <nickesc.media@gmail.com>
2022-06-03 04:32:34 -0400

    terminal input! and help and more!

---
+++ scripts/terminal.js
@@ -0,0 +1,121 @@
+class Terminal{
+
+
+
+    terminal = document.getElementById("terminal")
+    label = document.getElementById("terminalLabel")
+    active = true
+
+    lastCommands = []
+    commandIndex = 0
+    command;
+    promptDefault = "nickesc.github.io [Version 0.3.7]";
+    prompt = this.promptDefault;
+
+    constructor(terminalDisplay) {
+
+        this.promptUnknown = document.createElement("div")
+        this.promptUnknown.className = "errorText"
+        this.promptUnknown.innerText = "Unknown command. Click for help."
+
+        this.promptStopped = document.createElement("div")
+        this.promptStopped.className = "errorText"
+        this.promptStopped.innerText = "Stopped. Type 'start'."
+
+        this.terminalDisplay = terminalDisplay
+
+        this.init()
+    }
+
+
+
+
+
+    attachListeners(){
+        document.addEventListener('keypress', (event) => {
+            this.terminal.focus();
+        })
+        this.terminal.addEventListener('keypress', (event) => {
+
+            if (event.keyCode === 13 || event.code === "Enter") {
+                event.preventDefault();
+
+                this.command = this.terminal.value
+                console.log(this.command)
+                this.lastCommands.unshift(this.command)
+                this.terminal.value=""
+
+                this.commandInput(this.command)
+            }
+        });
+    }
+
+    init(){
+        this.attachListeners()
+    }
+
+    start(){
+        document.getElementsByClassName("terminalDisplay")[0].style.display = "block"
+        if(this.active === false) {
+            this.terminalDisplay.setDisplay(this.terminalDisplay.home)
+            this.terminalDisplay.toolTitle.style.color = "#232326"
+            this.terminal.placeholder="Type 'open help' for help."
+        }
+        this.active = true
+    }
+    exit(){
+        document.getElementsByClassName("terminalDisplay")[0].style.display = "none"
+        this.terminalDisplay.toolTitle.innerText = "STOPPED"
+        this.terminalDisplay.toolTitle.style.color = "#EF5252"
+
+        this.terminal.placeholder="Stopped. Type 'start' to start."
+
+        this.active = false
+    }
+
+    commandInput(command) {
+        this.prompt = this.promptDefault
+        this.promptUnknown.style.display = "none"
+
+        if(this.active || command === "start"){
+            switch (command) {
+                case "help":
+                    this.terminalDisplay.setDisplay(this.terminalDisplay.help)
+                    break;
+                case "open home":
+                    this.terminalDisplay.setDisplay(this.terminalDisplay.home)
+                    break;
+                case "open projects":
+                    this.terminalDisplay.setDisplay(this.terminalDisplay.projects)
+                    break;
+                case "open spiral":
+                    this.terminalDisplay.setDisplay(this.terminalDisplay.spiral)
+                    break;
+                case "open portfolio":
+                    this.terminalDisplay.redirectToPortfolio()
+                    break;
+                case "":
+                    break;
+                case "start":
+                    this.start()
+                    break;
+                case "stop":
+                    this.exit()
+                    break;
+
+                default:
+                    this.label.appendChild(this.promptUnknown)
+                    this.promptUnknown.style.display = "block"
+                    break;
+            }
+        } else{
+            this.label.appendChild(this.promptStopped)
+            this.promptUnknown.style.display = "none"
+            this.promptUnknown.style.display = "block"
+        }
+
+
+
+
+    }
+}
--- scripts/terminalDisplay.js
+++ scripts/terminalDisplay.js
@@ -1,17 +1,41 @@
 class TerminalDisplay {
 
+    urlBase = "nickesc.github.io"
+
+    toolApp = document.getElementById("toolApp")
+    toolTitle = document.getElementById("toolTitle")
+    toolTime = document.getElementById("toolTime")
+
+    errors = document.getElementsByClassName("errorText")
+
     homePage = document.getElementById("homePage")
     spiralPage = document.getElementById("spiralBlog")
     projectsPage = document.getElementById("projectsPage")
+    helpPage = document.getElementById("helpPage")
 
     homeButton =document.getElementById("homeButton")
     spiralButton = document.getElementById("spiralButton")
     projectsButton = document.getElementById("projectsButton")
     portfolioButton = document.getElementById("portfolioButton")
+    helpButton = document.getElementById("terminalLabel")
 
-    home = {page:this.homePage, button:this.homeButton}
-    spiral = {page:this.spiralPage, button:this.spiralButton}
-    projects = {page:this.projectsPage, button:this.projectsButton}
+    home = {page:this.homePage, button:this.homeButton, name:"home"}
+    spiral = {page:this.spiralPage, button:this.spiralButton, name:"spiral"}
+    projects = {page:this.projectsPage, button:this.projectsButton, name:"projects"}
+    help = {page:this.helpPage, button:this.helpButton, name:"help"}
+
+    display = {}
+
+    setTime(){
+        let today = new Date();
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+        let hours = today.getHours(); if(hours<10) hours = "0" + hours
+        let minutes = today.getMinutes(); if(minutes<10) minutes = "0" + minutes
+        let time = hours + ":" + minutes;
+
+        this.toolTime.innerText = date+' '+time;
+    }
 
     clearMain(){
         this.homePage.style.display = "none"
@@ -20,32 +44,57 @@ class TerminalDisplay {
         this.lightenButton(this.spiralButton)
         this.projectsPage.style.display = "none"
         this.lightenButton(this.projectsButton)
+        this.helpPage.style.display = "none"
+        for(let i =0;i<this.errors.length;i++){
+            this.errors[i].style.display = "none"
+        }
+    }
+
+    redirectToPortfolio(){
+        window.location.href = "http://nickesc.com";
     }
 
     attachListeners(){
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
         this.homeButton.addEventListener('click', (event) => {
-            //this.setPage('home')
             this.setDisplay(this.home)
 
         });
         this.spiralButton.addEventListener('click', (event) => {
-            //this.setPage('spiral')
             this.setDisplay(this.spiral)
         });
         this.projectsButton.addEventListener('click', (event) => {
-            //this.setPage('projects')
             this.setDisplay(this.projects)
+        });
+        this.helpButton.addEventListener('click', (event) => {
+            this.setDisplay(this.help)
         });
 
         this.portfolioButton.addEventListener('click', (event) => {
-            //this.setPage('projects')
-            window.location.href = "http://nickesc.com";
+            this.redirectToPortfolio();
         });
+
+
+
+        let buttons = document.getElementsByClassName("menuButton")
+
+        for(let button = 0; button<buttons.length; button++){
+            buttons[button].addEventListener('mouseenter', (event) => {
+                this.darkenButton(buttons[button])
+            });
+            buttons[button].addEventListener('mouseleave', (event) => {
+                if(this.display.button!==buttons[button]) {
+                    this.lightenButton(buttons[button])
+                }
+            });
+        }
     }
 
     darkenButton(button){
         button.style.backgroundColor = "#232326"
         button.style.color = "#FFF9F2"
+        console.log(button)
     }
     lightenButton(button){
         button.style.backgroundColor = "#FFF9F2"
@@ -53,23 +102,15 @@ class TerminalDisplay {
     }
 
     setDisplay(display){
-        //this.clearMain()
         console.log(display)
+        this.display = display
+        this.toolApp.innerText = this.urlBase.toUpperCase()
+        this.setTime()
         this.clearMain()
         display.page.style.display = "initial"
-        this.darkenButton(display.button)
-    }
-
-    setPage(page){
-        this.clearMain()
-        if(page === "home"){
-            this.homePage.style.display = "initial"
-        }
-        else if(page === "spiral"){
-            this.spiralPage.style.display = "initial"
-        }
-        else if(page === "projects"){
-            this.projectsPage.style.display = "initial"
+        this.toolTitle.innerText = ("/"+display.name).toUpperCase()
+        if(this.display!==this.help) {
+            this.darkenButton(display.button)
         }
     }
 
@@ -77,6 +118,8 @@ class TerminalDisplay {
         this.clearMain()
         this.attachListeners()
         this.setDisplay(this.home)
+        this.setTime()
+        setInterval(this.setTime, 1000*10);
     }
 
     constructor() {
