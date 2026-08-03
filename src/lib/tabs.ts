7b50f3fa0a4d70b8db2be9f84358f4a4525d0fd8
N. Escobar <nick@nickesc.io>
2026-08-03 04:10:09 -0400

    add filetree based on tabs

--- src/lib/tabs.ts
+++ src/lib/tabs.ts
@@ -1,3 +1,5 @@
+import type { Directory } from '$lib/filetree';
+
 export const tabs = [
 	{
 		name: 'Home',
@@ -24,3 +26,24 @@ export const tabs = [
 		href: 'https://nickesc.com/'
 	}
 ];
+
+const root: Directory = {
+	type: 'directory',
+	name: 'root',
+	path: '/',
+	parent: null,
+	children: []
+};
+
+export const tabTree: Directory = {
+	...root,
+	children: tabs
+		.filter((tab) => tab.href !== '/')
+		.map((tab): Directory => ({
+			type: 'directory',
+			name: tab.name,
+			path: tab.href,
+			parent: root,
+			children: []
+		}))
+};
