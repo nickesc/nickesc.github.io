97ae1e0fee108271d1768d836806acd9c8d47958
N. Escobar <nick@nickesc.io>
2026-08-03 04:15:58 -0400

    add function to get a dir or file from the path

--- src/lib/filetree.ts
+++ src/lib/filetree.ts
@@ -13,3 +13,14 @@ export type Directory = {
 	parent: Directory | null;
 	children: (File | Directory)[];
 };
+
+export function getFromPath(root: Directory, path: string): Directory | File {
+	if (path === '/') {
+		return root;
+	}
+	const target = root.children.find((child) => child.path === path);
+	if (!target) {
+		throw new Error(`Directory ${path} not found`);
+	}
+	return target;
+}
