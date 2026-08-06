e44eb9d71562425737f4a027b567aff297058658
N. Escobar <nick@nickesc.io>
2026-08-06 01:42:06 -0400

    add features projects to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { projects } from '$lib/projects';
+
 	import Badge from '$lib/components/Badge.svelte';
 
 	import SiHtml5 from '@icons-pack/svelte-simple-icons/icons/SiHtml5';
@@ -25,6 +27,36 @@
 	big fan of open source software, building unique experiences, and learning new things.
 </p>
 
+<h2>Check out some of my favorite projects:</h2>
+
+<div class="featured-projects-container">
+	{#each displayedProjects as project}
+		<a href={project.projectUrl} target="_blank" class="featured-container">
+			<div class="featured-logo-container" style="background-color: {project.background}">
+				<img class="featured-logo" src={project.imgUrl} alt={project.name} />
+			</div>
+
+			<h3 class="featured-name">{project.name}</h3>
+		</a>
+	{/each}
+</div>
+
+<h2>About me</h2>
+<p>School</p>
+<ul>
+	<li>
+		<span>Occidental College</span> <span>B.A. in Computer Science</span>
+		<span>Minor in Media studies</span> <span>Minor in Fine Art</span> <span>2018-2022</span>
+	</li>
+</ul>
+<p>Hobbies</p>
+<ul>
+	<li>video games</li>
+	<li>movies</li>
+	<li>collecting physical media</li>
+	<li>hiking</li>
+	<li>camping</li>
+</ul>
 <p>Tech</p>
 <ul class="badges">
 	<li>
@@ -81,6 +113,45 @@
 		margin: 0.75rem 0;
 	}
 
+	.featured-projects-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		gap: 1rem;
+		max-width: 900px;
+		margin: 0 auto;
+
+		.featured-container {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			justify-content: flex-start;
+			gap: 0.25rem;
+
+			.featured-logo-container {
+				width: 100%;
+				height: 100%;
+				overflow: hidden;
+				border-radius: var(--corners);
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+
+			.featured-logo {
+				width: 70%;
+				height: 100%;
+				object-fit: cover;
+			}
+
+			.featured-name {
+				font-size: 1.25rem;
+				font-weight: 700;
+				color: var(--brand-white);
+				margin: 0;
+				padding: 0;
+			}
+		}
+	}
 
 	.badges {
 		display: flex;
