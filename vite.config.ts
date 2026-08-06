7fe1618c26d020817207ed69c102e4903adf26e3
N. Escobar <nick@nickesc.io>
2026-08-06 01:18:59 -0400

    add svelte simple icons

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,7 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter({
-				// default options are shown. On some platforms
-				// these options are set automatically — see below
 				pages: 'build',
 				assets: 'build',
 				fallback: undefined,
@@ -24,5 +19,8 @@ export default defineConfig({
 				strict: true
 			})
 		})
-	]
+	],
+	ssr: {
+		noExternal: ['@icons-pack/svelte-simple-icons']
+	}
 });
