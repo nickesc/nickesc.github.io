79c9d13b2b3a65144a435426ed144a7b1122a36e
N. Escobar <nick@nickesc.io>
2026-08-07 12:55:47 -0400

    add archived projects filter

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { projects } from '$lib/projects';
 	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import { onMount } from 'svelte';
 
 	let displayedProjects = $state(projects);
 	let selectedCategory = $state('all');
 	let selectedYear: number | 'all' = $state<number | 'all'>('all');
+	let showArchived = $state(false);
 
 	let delay = 0;
 
@@ -14,41 +16,56 @@
 	);
 
 	function filterProjects() {
-		if (selectedCategory === 'all' && selectedYear === 'all') {
+		if (selectedCategory === 'all' && selectedYear === 'all' && showArchived) {
 			displayedProjects = projects;
 		} else {
 			displayedProjects = projects.filter(
 				(project) =>
 					(selectedCategory === 'all' || project.category === selectedCategory) &&
-					(selectedYear === 'all' || project.year === selectedYear)
+					(selectedYear === 'all' || project.year === selectedYear) &&
+					(showArchived || !project.archived)
 			);
 		}
 	}
+
+	onMount(() => {
+		filterProjects();
+	});
 </script>
 
-<h2>Projects</h2>
+<span class="projects-title">
+	<h2>Projects</h2>
+	<span class="projects-count">{displayedProjects.length} items</span>
+</span>
 
 <div class="filters">
-	<label for="category"
-		>Category
-		<select bind:value={selectedCategory} onchange={filterProjects}>
-			<option value="all">All</option>
-			{#each categories as category}
-				<option value={category}
-					>{category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}</option
-				>
-			{/each}
-		</select>
-	</label>
+	<span class="filters-selects">
+		<label for="category"
+			>Category
+			<select bind:value={selectedCategory} onchange={filterProjects}>
+				<option value="all">All</option>
+				{#each categories as category}
+					<option value={category}
+						>{category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}</option
+					>
+				{/each}
+			</select>
+		</label>
 
-	<label for="year"
-		>Year
-		<select bind:value={selectedYear} onchange={filterProjects}>
-			<option value="all">All</option>
-			{#each years as year}
-				<option value={year}>{year}</option>
-			{/each}
-		</select>
+		<label for="year"
+			>Year
+			<select bind:value={selectedYear} onchange={filterProjects}>
+				<option value="all">All</option>
+				{#each years as year}
+					<option value={year}>{year}</option>
+				{/each}
+			</select>
+		</label>
+	</span>
+
+	<label for="archived">
+		Show Archived
+		<input type="checkbox" bind:checked={showArchived} onchange={filterProjects} />
 	</label>
 </div>
 
@@ -59,6 +76,20 @@
 </div>
 
 <style>
+	.projects-title {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 1rem;
+	}
+	.projects-count {
+		font-size: 14px;
+		color: rgba(from var(--brand-grey) r g b / 0.7);
+		font-family: var(--mono-font);
+		letter-spacing: 0.05em;
+		text-transform: uppercase;
+	}
+
 	h2 {
 		margin: 0;
 		margin-bottom: 1rem;
@@ -72,6 +103,13 @@
 		display: flex;
 		gap: 1rem;
 		margin-bottom: 1rem;
+		justify-content: space-between;
+
+		.filters-selects {
+			display: flex;
+			gap: 1rem;
+			align-items: end;
+		}
 
 		label {
 			display: flex;
@@ -83,8 +121,9 @@
 			letter-spacing: 0.05em;
 			font-family: var(--mono-font);
 
-			.required {
-				color: rgba(199, 32, 66, 0.8);
+			&[for='archived'] {
+				align-items: flex-end;
+				text-align: right;
 			}
 		}
 	}
