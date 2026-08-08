bb6a4eaa6fc36cd63244b122cb546bc28547911e
N. Escobar <nick@nickesc.io>
2026-08-08 03:58:18 -0400

    add icons to available badges

--- src/lib/components/Badge.svelte
+++ src/lib/components/Badge.svelte
@@ -9,6 +9,19 @@
 	import SiPython from '@icons-pack/svelte-simple-icons/icons/SiPython';
 	import SiZsh from '@icons-pack/svelte-simple-icons/icons/SiZsh';
 	import SiGodotengine from '@icons-pack/svelte-simple-icons/icons/SiGodotengine';
+	import SiBluesky from '@icons-pack/svelte-simple-icons/icons/SiBluesky';
+	import SiGithub from '@icons-pack/svelte-simple-icons/icons/SiGithub';
+	import SiKofi from '@icons-pack/svelte-simple-icons/icons/SiKofi';
+	import SiYoutube from '@icons-pack/svelte-simple-icons/icons/SiYoutube';
+	import SiNpm from '@icons-pack/svelte-simple-icons/icons/SiNpm';
+	import SiFirefox from '@icons-pack/svelte-simple-icons/icons/SiFirefox';
+	import SiItchdotio from '@icons-pack/svelte-simple-icons/icons/SiItchdotio';
+	import SiModrinth from '@icons-pack/svelte-simple-icons/icons/SiModrinth';
+	import InfosecExchange from '$lib/components/icons/infosec-exchange.svelte';
+	import Linkedin from '$lib/components/icons/linkedin.svelte';
+	import OpenVsx from '$lib/components/icons/open-vsx.svelte';
+	import Sig from '$lib/components/icons/sig.svelte';
+	import VsMp from '$lib/components/icons/vs-mp.svelte';
 
 	const icons = {
 		html: SiHtml5,
@@ -20,7 +33,20 @@
 		node: SiNodedotjs,
 		python: SiPython,
 		zsh: SiZsh,
-		godotengine: SiGodotengine
+		godotengine: SiGodotengine,
+		bluesky: SiBluesky,
+		github: SiGithub,
+		'infosec-exchange': InfosecExchange,
+		kofi: SiKofi,
+		linkedin: Linkedin,
+		'open-vsx': OpenVsx,
+		sig: Sig,
+		'vs-mp': VsMp,
+		youtube: SiYoutube,
+		npm: SiNpm,
+		firefox: SiFirefox,
+		itchio: SiItchdotio,
+		modrinth: SiModrinth
 	} as const;
 
 	interface Props {
@@ -36,7 +62,7 @@
 </script>
 
 <a {href} style:--logo-color={bgColor} style:color={bgColor} target="_blank">
-	<Icon class="icon" title={text} />
+	<Icon class="icon" title={text} color={bgColor} />
 
 	{text}
 </a>
