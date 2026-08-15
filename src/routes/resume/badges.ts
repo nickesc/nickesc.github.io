868d9cb2c80531b8a30d28a01a651866c22bb653
N. Escobar <nick@nickesc.io>
2026-08-15 16:49:43 -0400

    move specific badges from the component to a ts file

--- src/routes/resume/badges.ts
+++ src/routes/resume/badges.ts
@@ -1,1 +1,9 @@
+import { badges } from '$lib/badges';
+
 export { default as Badge } from '$lib/components/Badge.svelte';
+
+export type BadgeKey = keyof typeof badges;
+
+export const techBadges = (Object.keys(badges) as BadgeKey[]).filter((key) =>
+	badges[key].categories.includes('tech')
+);
