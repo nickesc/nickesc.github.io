f564e93ffe2cc0ababe018f424c7638b77dd9497
N. Escobar <nick@nickesc.io>
2026-09-07 21:31:58 -0400

    add spotify repsonse shape for mgic

---
+++ src/lib/spotifyResponse.ts
@@ -0,0 +1,26 @@
+export type SpotifyResponse = {
+	playing: boolean;
+	device?: { name: string; type: string };
+	player?: {
+		vol: number;
+		shuffle: boolean;
+		repeat: 'off' | 'track' | 'context';
+		progress: number;
+	};
+	track?: {
+		context: {
+			name: string;
+			type: string;
+			url: string;
+		} | null;
+		artists: {
+			names: string[];
+			url: string;
+		};
+		contentType: 'track' | 'episode';
+		name: string;
+		image: string;
+		explicit: boolean;
+		url: string;
+	};
+};
