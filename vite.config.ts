b6756c63346d8d6251a38edfe6be7694a1c1a318
N. Escobar <nick@nickesc.io>
2026-09-18 11:01:45 -0400

    use default import for package

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import rehypeExternalLinks from 'rehype-external-links';
 import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { version } from './package.json' with { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
 	plugins: [
@@ -38,6 +38,6 @@ export default defineConfig({
 			extensions: ['.svelte', '.svx', '.md']
 		})
 	],
-	define: { __APP_VERSION__: JSON.stringify(version) },
+	define: { __APP_VERSION__: JSON.stringify(pkg.version) },
 	ssr: { noExternal: ['@icons-pack/svelte-simple-icons'] }
 });
